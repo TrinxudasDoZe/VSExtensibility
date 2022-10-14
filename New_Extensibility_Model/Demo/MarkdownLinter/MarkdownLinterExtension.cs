52753c85a2610a1ef8b10a74f439811383f41d65
Bertan Aygun <bertaygu@microsoft.com>
2022-10-13 19:52:52 -0700

    Demo project post commands sessions

---
+++ New_Extensibility_Model/Demo/MarkdownLinter/MarkdownLinterExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensions.MarkdownLinter;
+
+namespace MarkdownLinter
+{
+	/// <summary>
+	/// Entry class for markdown linter extension.
+	/// </summary>
+	internal class MarkdownLinterExtension : Extension
+	{
+		protected override void InitializeServices(IServiceCollection serviceCollection)
+		{
+			base.InitializeServices(serviceCollection);
+			serviceCollection.AddScoped<MarkdownDiagnosticsService>();
+		}
+	}
+}
---
+++ New_Extensibility_Model/Demo/MarkdownLinter/RunLinterOnCurrentFileCommand.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using Microsoft;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+using Microsoft.VisualStudio.Extensibility.Definitions;
+using Microsoft.VisualStudio.Extensions.MarkdownLinter;
+
+namespace MarkdownLinter
+{
+	[CommandIcon(KnownMonikers.Extension, IconSettings.IconAndText)]
+	[Command(
+		"MarkdownExtension.RunLinterOnCurrentFile",
+		"Run markdown linter on current file",
+		placement: CommandPlacement.ToolsMenu)]
+	[CommandEnabledWhen(
+		"FileSelected",
+		new string[] { "FileSelected"},
+		new string[] { "ClientContext:Shell.ActiveSelectionFileName=.md$" })]
+	internal class RunLinterOnCurrentFileCommand : Command
+	{
+		private TraceSource traceSource;
+		private readonly MarkdownDiagnosticsService markdownDiagnosticsService;
+
+		public RunLinterOnCurrentFileCommand(
+			VisualStudioExtensibility extensibility,
+			TraceSource traceSource,
+			MarkdownDiagnosticsService markdownDiagnosticsService,
+			string id)
+			: base(extensibility, id)
+		{
+			this.traceSource = Requires.NotNull(traceSource, nameof(traceSource));
+			this.markdownDiagnosticsService = Requires.NotNull(markdownDiagnosticsService, nameof(markdownDiagnosticsService));
+		}
+
+		public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+
+			try
+			{
+				// Get the selected item URIs from IDE context that reprents the state when command was executed.
+				var selectedItemPaths = new Uri[] { await context.GetSelectedPathAsync(cancellationToken) };
+
+				// Enumerate through each selection and run linter on each selected item.
+				foreach (var selectedItem in selectedItemPaths.Where(p => p.IsFile))
+				{
+					await this.markdownDiagnosticsService.ProcessFileAsync(selectedItem, cancellationToken);
+				}
+			}
+			catch (InvalidOperationException ex)
+			{
+				this.traceSource.TraceEvent(TraceEventType.Error, 0, ex.ToString());
+			}
+		}
+	}
+}
